11e9d39e199857ddbbd64557d1dd79fcdcf83250
zouxiaoyu <weiqinzou@qq.com>
2016-12-28 19:30:20 +0800

    record filtered fn for filter2 with closed PR issues

--- filter_PRissues.sh
+++ filter_PRissues.sh
@@ -7,9 +7,10 @@ prLimit=200 #closed PR number >=200
 issueLimit=20 #closed issues number >=20
 
 cnt=1
-rm failed prIssueOk_fn
+rm failed prIssueOk_fn filteredFn
 for fn in $(cat $fnList)
 do
+    echo $fn >> filteredFn
     clientNum=$((cnt%clientCnt + 1))
     client=$(sed -n "${clientNum}p" $clientAccount)
     issueUrl="https://api.github.com/search/issues?q=type:issue+repo:${fn}+state:closed&${client}&per_page=1"
